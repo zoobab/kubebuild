358695b7935d3745ea22809358c2139a282207e0
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2018-02-06 15:35:26 +0100

    add a kubebuild-12345 build_number

--- kubebuild.sh
+++ kubebuild.sh
@@ -18,6 +18,7 @@ FINAL="$NAME.yaml"
 TEMPLATE="$NAME.tmpl"
 FINAL_PATH="$WORKDIR/$FINAL"
 GIT_URL_REGEX="http?(s)://*.git"
+BUILD_NUMBER="$(echo $RANDOM)"
 
 if [[ "$REPO_URL" != $GIT_URL_REGEX ]]; then
     echo "ERROR, invalid URL (not with http(s):// ending by .git)"
@@ -29,23 +30,25 @@ REPONAME_DOTGIT="$(basename $REPO_URL)"
 REPO_NAME=$(echo $REPONAME_DOTGIT | sed -e "s/.git//g")
 
 echo -ne "[1/4] Templating (with repo named '$REPO_NAME')..."
-sed -e "s#{{{repo_url}}}#$REPO_URL#g" -e "s#{{{repo_name}}}#$REPO_NAME#g" $TEMPLATE > $FINAL_PATH
+sed -e "s#{{{repo_url}}}#$REPO_URL#g" -e "s#{{{repo_name}}}#$REPO_NAME#g" -e "s#{{{buildnumber}}}#$BUILD_NUMBER#g" $TEMPLATE > $FINAL_PATH
 echo -ne "OK\n"
 
-echo "[2/4] Launching in kubernetes..."
+POD_NAME="kubebuild-$BUILD_NUMBER"
+
+echo "[2/4] Launching pod '$POD_NAME' in kubernetes..."
 kubectl apply -f $FINAL_PATH
 
 get_pod_status() {
-    POD_STATUS=$(kubectl get pod kubebuild | tail -n +2 | awk '{print $3}')
+    POD_STATUS=$(kubectl get pod $POD_NAME | tail -n +2 | awk '{print $3}')
     echo $POD_STATUS
 }
 
 POD_STATUS=$(get_pod_status)
 while [ "$POD_STATUS" != "Running" ] ; do
-    echo "[3/4] Waiting for pod, current status $POD_STATUS"
+    echo "[3/4] Waiting for pod '$POD_NAME', current status $POD_STATUS"
     POD_STATUS=$(get_pod_status)
     sleep 2
 done
 
 echo "[4/4] Tail the log..."
-kubectl logs -f kubebuild
+kubectl logs -f $POD_NAME
